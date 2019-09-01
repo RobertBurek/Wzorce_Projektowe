bbc043cc874ac34bf437447d6fcc728490130d81
Robert Burek <robertburek@wp.pl>
2019-09-01 16:19:25 +0200

    Wzorzec kreacyjny - SINGLETON - początek

---
+++ Singleton/src/main/java/pl/robertburek/singleton/GameEngine.java
@@ -0,0 +1,16 @@
+package pl.robertburek.singleton;
+
+public class GameEngine {
+
+    private int hp = 100;
+    private String characterName = "";
+
+    public void run() {
+        while(true) {
+            //czekamy na input gracza
+            //zmienamy stan gry
+            //renderujemy grafike
+        }
+    }
+
+}
