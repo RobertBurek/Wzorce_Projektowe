6e52a9692cd4ebdb18460a44e4689a3f2d9d8c70
Robert Burek <robertburek@wp.pl>
2019-09-01 22:35:11 +0200

    Wzorzec kreacyjny - BUILDER - klasa wewnętrzna

--- Builder/src/main/java/pl/robertburek/Main.java
+++ Builder/src/main/java/pl/robertburek/Main.java
@@ -7,10 +7,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        House house1 = new House("walls", "floors", "rooms", "windows", "doors", "garage");
+//        House house1 = new House("walls", "floors", "rooms", "windows", "doors", "garage");
 //        House house2 = new House("")
 
+        House house = new House.HouseBuilder()
+                .buildDoors("doors")
+                .buildFloors("floors")
+                .buildRoof("roof")
+                .buildGarage("garage")
+                .build();
 
+        House house2 = new House.HouseBuilder()
+                .build();
+
+        House house3 = new House.HouseBuilder()
+                .buildRooms("rooms")
+                .buildDoors("doors")
+                .buildWindows("windows")
+                .build();
+
+        System.out.println(house);
+        System.out.println(house2);
+        System.out.println(house3);
     }
 
 }
