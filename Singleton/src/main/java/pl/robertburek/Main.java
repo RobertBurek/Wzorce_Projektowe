4c7e1205e08e976acfc726cc0a9ca802d838001d
Robert Burek <robertburek@wp.pl>
2019-09-01 16:38:08 +0200

    Wzorzec kreacyjny - SINGLETON - podstawowa implementacja
       Prywatne, statyczne pole - instance, publiczna, statyczna metoda get,
       sprawdzająca istnieje instancji, bezargumentowy konstruktor.
       Dalej cztery kruczki implementacyjne!!!

--- Singleton/src/main/java/pl/robertburek/Main.java
+++ Singleton/src/main/java/pl/robertburek/Main.java
@@ -1,9 +1,15 @@
 package pl.robertburek;
 
+import pl.robertburek.singleton.GameEngine;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        GameEngine engine = GameEngine.getInstance();
+        GameEngine engine2 = GameEngine.getInstance();
+
+        System.out.println("To są te same instancje: " + (engine == engine2));
     }
 
 }
--- Singleton/src/main/java/pl/robertburek/singleton/GameEngine.java
+++ Singleton/src/main/java/pl/robertburek/singleton/GameEngine.java
@@ -5,8 +5,20 @@ public class GameEngine {
     private int hp = 100;
     private String characterName = "";
 
+    private static GameEngine instance;
+
+    private GameEngine() {
+    }
+
+    public static GameEngine getInstance() {
+        if (instance == null) {
+            instance = new GameEngine();
+        }
+        return instance;
+    }
+
     public void run() {
-        while(true) {
+        while (true) {
             //czekamy na input gracza
             //zmienamy stan gry
             //renderujemy grafike
