149a8092d5a30d193a81c4ee025e1dd46c14ac32
Robert Burek <robertburek@wp.pl>
2019-09-01 17:19:20 +0200

    Wzorzec kreacyjny - SINGLETON - wielowątkowość
       trzy sposoby rozwiązania problemu wielowątkowości

--- Singleton/src/main/java/pl/robertburek/singleton/GameEngine.java
+++ Singleton/src/main/java/pl/robertburek/singleton/GameEngine.java
@@ -5,15 +5,20 @@ public class GameEngine {
     private int hp = 100;
     private String characterName = "";
 
+    // 3) private static GameEngine instance = new GameEngine();
+    // 3) tworzenie instancji w lini deklaracji, wtedy getInstance() tylko zwraca instance;
     private static GameEngine instance;
 
     private GameEngine() {
     }
 
+    //    public synchronized static GameEngine getInstance() { //1) synchronizacja na całą metodę
     public static GameEngine getInstance() {
-        if (instance == null) {
-            instance = new GameEngine();
-        }
+        if (instance == null) {                                                                                 //3) usuń
+            synchronized (GameEngine.class) { //2) dodatkowy blok synchronizacji tylko na tworzenie instancji  //3) usuń
+                instance = new GameEngine();                                                                    //3) usuń
+            }                                 //2) dodatkowy blok synchronizacji tylko na tworzenie instancji   //3) usuń
+        }                                                                                                       //3) usuń
         return instance;
     }
 
